eabdcce82f4f2eb9408a2f8e542149a223a401d4
Stefan Ziegler <edi.gonzales@gmail.com>
2015-03-15 19:04:33 +0100

    erste versuche

---
+++ app.py
@@ -0,0 +1,11 @@
+#!flask/bin/python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/heights', methods=['GET'])
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
